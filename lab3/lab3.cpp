a80a2ec19f68872df9ecd3d4f2eb86fd7242884f
006084880 <006084880@csusb.edu@csevnc.cse.csusb.edu>
2018-02-06 18:53:59 -0800

    lab3

---
+++ lab3/lab3.cpp
@@ -0,0 +1,81 @@
+/*****************************************************************************
+ * Paul Alvarez
+ * CSE 330 section 2
+ * lab3.cpp
+ * 2/6/2018
+ * AAn implementation of a priority queue
+ ***************************************************************************/
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <time.h>
+using namespace std;
+
+template <typename T>
+class PQueue
+{
+	public:
+	PQueue();
+	int  qsize();
+	T   front();
+	void print();
+	void add(T x);
+        void remove();
+
+	private:
+	PQueue(vector<T> &v);
+	vector<T> vec;
+};
+
+template <typename T> PQueue<T>::PQueue()
+{ 
+}
+
+template <typename T> PQueue<T>::PQueue(vector<T> &v)
+{
+for (int i = v.size()-1; i > 0; i--)
+    for (int j = 0; j < i; j++)
+        if (v[j] > v[j+1])
+            swap(v[j], v[j+1]);
+}
+
+template<typename T> int PQueue<T>::qsize()
+{  int size;
+   size=vec.size();
+  return size;   
+}
+
+template<typename T> T PQueue<T>::front()
+{      T value;
+ 	value= vec.front();
+	return value;
+}
+
+template<typename T> void PQueue<T>::print()
+{
+  for (int i = 0; i < vec.size(); i++)
+        cout<<vec[i]<<endl;
+}
+
+template<typename T> void PQueue<T>::add(T x)
+{  
+   vec.push_back(x);
+   PQueue<T> srt(vec);
+}
+
+template<typename T> void PQueue<T>::remove()
+{
+       	vec.erase(vec.begin());
+}
+
+int main()
+{
+   PQueue<double> q;
+   q.add(3.54);
+   q.add(4.76);
+   q.add(1.00);
+   q.print();
+   q.remove();
+   q.print();
+}
