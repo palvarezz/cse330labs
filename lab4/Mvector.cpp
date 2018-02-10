72bf88a2a9f74eea0ec12aca4b9d254ffcc2766f
006084880 <006084880@csusb.edu@csevnc.cse.csusb.edu>
2018-02-09 23:37:52 -0800

    added functions

--- lab4/Mvector.cpp
+++ lab4/Mvector.cpp
@@ -21,54 +21,101 @@ class Mvector
 
 };
 
-template<typename T> Mvector<T>::Mvector(){}
+template<typename T> Mvector<T>::Mvector()
+{
+	T *newv=new T[5];
+	vcap=5;
+	vsize=0;
+	v=newv;
+}
 template<typename T> Mvector<T>::Mvector(unsigned int n)
 {
 
+	T *newv=new T[n];
+	vcap=n;
+	vsize=0;
+	v=newv;
 }
 
 
 template<typename T> void Mvector<T>::pushback(T x)
 {
-
+       if(vcap==vsize)
+       {
+ 	 reserve(vcap*2);
+       }  
+       	v[vsize]=x;
+	  vsize++;
 }
 
 template<typename T> void Mvector<T>::popback()
 {
-
+         v[vsize]=0;
+	 vsize--;
 }
 
 template<typename T> void Mvector<T>::clear()
 {
-
+     for(int i=0;i<vsize;i++)
+     {
+	v[i]=0;
+     }
+     vsize=0;
 }
 
 template<typename T> void Mvector<T>::insert(int i, T x)
 {
-
+     if(vsize==vcap)
+     {
+	reserve(vcap*2);
+     }
 }
 
 template<typename T>void Mvector<T>::erase(int i)
 {
-
+    for(int j=i;j<vsize;i++)
+    {
+	v[j]=v[j+1];
+    }
+   
 }
 
 template<typename T> T Mvector<T>::operator[](unsigned int i)
 {
-
+   return v[i]; 
 }
 
 template<typename T> int Mvector<T>::size()
 {
- 
+   return vsize; 
 }
 
 template <typename T> void Mvector<T>::reserve(unsigned int n)
 {
-
+     T* rv=new T[n];
+     vcap=n;
+    // vsize=0;
+     for(int i=0;i<vsize;i++)
+     {
+	rv[i]=v[i];
+     }
+     delete v;
+     v=rv;
 }
 int main()
-{
-  
-
+{	int ind;
+	Mvector<int> v;
+	v.pushback(3);
+	v.pushback(2);
+	v.pushback(5);
+	v.pushback(7);
+	v.pushback(8);
+	v.pushback(19);
+	v.pushback(20);
+	//v.clear();
+	for(int i=0;i<v.size();i++)
+	{
+	ind=v[i];
+	cout<<ind<<endl;
+	}
 }
